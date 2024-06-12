8d3b93c90b675f881f347ef5a1e5ba237744f3c5
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-12 13:08:34 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    for(int x = 0; x < matrixSize; x++)
+    {
+        const int half = matrixSize - 1 - x;
+        for(int y = x; y < half; y++)
+        {
+            const int offset = y - x;
+            const int top = matrix[x][y];
+            matrix[x][y] = matrix[half - offset][x];
+            matrix[half - offset][x] = matrix[half][half - offset];
+            matrix[half][half - offset] = matrix[y][half];
+            matrix[y][half] = top;
+        }
+    }
+}
